6296f4452341c85715cf0aa756361d0e6c027d0c
Taka <71265675+takakv@users.noreply.github.com>
2024-09-04 19:07:30 +0300

    Generate tokens

--- tokens.py
+++ tokens.py
@@ -1,3 +1,4 @@
+import csv
 import os
 from datetime import datetime
 
@@ -7,13 +8,26 @@ import jwt
 load_dotenv()
 SECRET = os.getenv("JWT_SECRET")
 
-def main():
-    encoded = jwt.encode({
-        "exp": datetime(2024, 9, 16),
-        "name": "Firstname Lastname",
-        "studentCode": "test123"
+
+def get_jwt(name: str, student_code: str) -> str:
+    return jwt.encode({
+        "exp": datetime(2024, 9, 10),
+        "name": name,
+        "studentCode": student_code,
     }, SECRET, algorithm="HS256")
-    print(encoded)
+
+
+def main():
+    with open("students.csv") as sf, open("tokens.csv", "a") as tf:
+        csv_reader = csv.reader(sf, delimiter=";")
+        read_header = False
+        for row in csv_reader:
+            if not read_header:
+                read_header = True
+                continue
+
+            token = get_jwt(f"{row[2]} {row[3]}", row[1])
+            tf.write(f"{row[2]} {row[3]};{row[1]};{token}\n")
 
 
 if __name__ == "__main__":
